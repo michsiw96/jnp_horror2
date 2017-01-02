91949caa5b1869b36c982e6b9a08f92ec6901018
Michał Siwiński <ms371763@students.mimuw.edu.pl>
2017-01-02 23:28:17 +0100

    monster.h (compiles / not tested)

--- monster.h
+++ monster.h
@@ -1,17 +1,78 @@
 #ifndef MONSTER_H
 #define MONSTER_H
 
-#include "helper.h"
+#include <vector>
+#include <functional>
+#include <algorithm>
 
 class Monster {
+protected:
+	HealthPoints health;
+	AttackPower damage;
 public:
+	Monster(HealthPoints health, AttackPower attackPower)
+	: health(health), damage(attackPower) { }
+	
 	HealthPoints getHealth() {
-		return HealthPoints(0);
+		return health;
 	}
-
-	Age getAge() {
-		return Age(0);
+	
+	AttackPower getAttackPower() {
+		return damage;
+	}
+	
+	void takeDamage(AttackPower damage) {
+		health.setHealth(std::max(health.getHealth() - damage.getAttackPower(), static_cast<double>(0)));
 	}
 };
 
-#endif
+class Zombie : public Monster {
+public:
+	Zombie(HealthPoints health, AttackPower attackPower) 
+		: Monster(health, attackPower) { }
+};
+
+class Vampire : public Monster {
+public:
+	Vampire(HealthPoints health, AttackPower attackPower) 
+		: Monster(health, attackPower) { }
+};
+
+class Mummy : public Monster {
+public:
+	Mummy(HealthPoints health, AttackPower attackPower) 
+		: Monster(health, attackPower) { }
+};
+
+class GroupOfMonsters {
+public:
+	std::vector<Monster> monsters;
+	
+	GroupOfMonsters(const std::vector<Monster> &monsters) : monsters(monsters) { }
+	
+	HealthPoints getHealth() {
+		int sum = 0;
+		for (Monster x : monsters) {
+			sum += x.getHealth().getHealth();
+		}
+		
+		return sum;
+	}
+	
+	AttackPower getAttackPower() {
+		int sum = 0;
+		for (Monster x : monsters) {
+			sum += x.getAttackPower().getAttackPower();
+		}
+		
+		return sum;
+	}
+	
+	void takeDamage(AttackPower damage) {
+		for (Monster x : monsters) {
+			x.takeDamage(damage);
+		}
+	}
+};
+
+#endif /* MONSTER_H */
